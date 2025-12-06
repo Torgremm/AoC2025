4bc924a997f1a1ff96baa732e89cfa6101598c8f
torgremm-admin <tormodnes@gmail.com>
2025-12-06 14:59:36 +0100

    day 3

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,24 @@ mod solutions;
 
 fn main() {
 
-    let day1_1 = solutions::day1::get_answer1();
-    let day1_2 = solutions::day1::get_answer2();
+    // let day1_1 = solutions::day1::get_answer1();
+    // let day1_2 = solutions::day1::get_answer2();
 
-    let day2_1 = solutions::day2::get_answer1();
-    let day2_2 = solutions::day2::get_answer2();
+    // let day2_1 = solutions::day2::get_answer1();
+    // let day2_2 = solutions::day2::get_answer2();
 
-    println!("Day 1:");
-    println!("\tPart 1: {day1_1}");
-    println!("\tPart 2: {day1_2}");
+    let day3_1 = solutions::day3::get_answer1();
+    let day3_2 = solutions::day3::get_answer2();
 
-    println!("Day 2:");
-    println!("\tPart 1: {day2_1}");
-    println!("\tPart 2: {day2_2}");
+    // println!("Day 1:");
+    // println!("\tPart 1: {day1_1}");
+    // println!("\tPart 2: {day1_2}");
+
+    // println!("Day 2:");
+    // println!("\tPart 1: {day2_1}");
+    // println!("\tPart 2: {day2_2}");
+
+    println!("Day 3:");
+    println!("\tPart 1: {day3_1}");
+    println!("\tPart 2: {day3_2}");
 }
