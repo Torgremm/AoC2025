e5155e99bc371f9107660fc560fff8ca8eff26ba
torgremm-admin <tormodnes@gmail.com>
2025-12-08 18:40:31 +0100

    day 8

--- src/main.rs
+++ src/main.rs
@@ -15,5 +15,5 @@ macro_rules! run_day {
 }
 
 fn main() {
-    run_day!(7);
+    run_day!(8);
 }
--- src/solutions/day8.rs
+++ src/solutions/day8.rs
@@ -1,28 +1,151 @@
 #![allow(dead_code)]
 
 use crate::solutions::sol_trait::Solution;
+use std::time::Instant;
 
 pub struct Day8;
 
+pub fn get_distance(a: &(i64, i64, i64), b: &(i64, i64, i64)) -> i64 {
+    let dx = a.0 - b.0;
+    let dy = a.1 - b.1;
+    let dz = a.2 - b.2;
+    dx * dx + dy * dy + dz * dz
+}
+
+pub fn connect(a: usize, b: usize, connections: &mut Vec<Vec<usize>>) {
+    let mut a_constellation: Option<usize> = None;
+    let mut b_constellation: Option<usize> = None;
+
+    for (i, constellation) in connections.iter().enumerate() {
+        if constellation.contains(&a) {
+            a_constellation = Some(i);
+        }
+        if constellation.contains(&b) {
+            b_constellation = Some(i);
+        }
+    }
+
+    match (a_constellation, b_constellation) {
+        (Some(ai), Some(bi)) => {
+            if ai != bi {
+                let mut b_constellation_data = connections[bi].clone();
+                connections[ai].append(&mut b_constellation_data);
+                connections.remove(bi);
+            }
+        }
+        (Some(ai), None) => {
+            connections[ai].push(b);
+        }
+        (None, Some(bi)) => {
+            connections[bi].push(a);
+        }
+        (None, None) => {
+            connections.push(vec![a, b]);
+        }
+    }
+
+}
+
+
 impl Solution for Day8{
     fn get_answer1() -> i64 {
-        crate::solve_with_time!(1, 0)
+        let start = Instant::now();
+        //assert_eq!(Self::solve1(&Self::get_example_input()), 40);
+        let sol = Self::solve1(&Self::get_input());
+        println!("Part 1 solved in: {:?}", start.elapsed());
+        sol
     }
 
     fn get_answer2() -> i64 {
-        crate::solve_with_time!(2, 0)
+        let start = Instant::now();
+        assert_eq!(Self::solve2(&Self::get_example_input()), 25272);
+        let sol = Self::solve2(&Self::get_input());
+        println!("Part 2 solved in: {:?}", start.elapsed());
+        sol
     }
 
     fn solve1(input: &str) -> i64 {
-        0
+        let data: Vec<(i64, i64, i64)> = input.lines().map(|line| {
+            let nums: Vec<i64> = line.split(',').map(|s| s.parse().unwrap()).collect();
+            (nums[0], nums[1], nums[2])
+        }).collect();
+
+        let length = data.len();
+        let mut edges: Vec<(i64,usize,usize)> = Vec::new();
+
+        for i in 0..length {
+            for j in (i+1)..length {
+                let dist = get_distance(&data[i], &data[j]);
+                edges.push((dist, i, j));
+            }
+        }
+
+        edges.sort_by(|a, b| a.0.cmp(&b.0));
+
+        let mut connections: Vec<Vec<usize>> = Vec::new();
+        for i in 0..1000 {
+            let (_, a, b) = edges[i];
+            connect(a, b, &mut connections);
+        }
+
+        let mut count: Vec<usize> = connections.iter().map(|d| d.len()).collect();
+        count.sort_by(|a, b| b.cmp(a));
+        (count[0] * count[1] * count[2]) as i64
+
     }
 
     fn solve2(input: &str) -> i64 {
+        let data: Vec<(i64, i64, i64)> = input.lines().map(|line| {
+            let nums: Vec<i64> = line.split(',').map(|s| s.parse().unwrap()).collect();
+            (nums[0], nums[1], nums[2])
+        }).collect();
+
+        let length = data.len();
+        let mut edges: Vec<(i64,usize,usize)> = Vec::new();
+
+        for i in 0..length {
+            for j in (i+1)..length {
+                let dist = get_distance(&data[i], &data[j]);
+                edges.push((dist, i, j));
+            }
+        }
+
+        edges.sort_by(|a, b| a.0.cmp(&b.0));
+
+        let mut connections: Vec<Vec<usize>> = Vec::new();
+        
+        for i in 0..edges.len() {
+            let (_, a, b) = edges[i];
+            connect(a, b, &mut connections);
+            if connections[0].len() == length {
+                return data[a].0 * data[b].0;
+            }
+        }
         0
     }
 
     fn get_example_input() -> String {
-        "".to_string()
+        "162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689
+".to_string()
     }
     
 }
